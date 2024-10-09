7bde271510c912de7514ebdb49448fb776902c32
isuruch92 <isuruch92@gmail.com>
2024-10-09 22:24:31 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use("/api/auth", authRoutes);
 } */
 
 app.listen(5000, () => {
-  // connectDB();
+  connectDB();
   console.log("Server is running on port: ", PORT);
   console.log("HELLO ISURU");
 });
